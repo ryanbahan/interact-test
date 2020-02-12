4749c367400ede0ca3d6f22287f514e097a8c244
Ryan Bahan <ryan@ryanbahan.com>
2020-02-11 17:10:43 -0700

    complete drag and drop functionality

--- main.js
+++ main.js
@@ -2,51 +2,74 @@ interact('.dropzone')
   .dropzone({
   })
   .on('drop', function (event) {
-    let rowA = document.querySelector('.row-a');
-    let rowB = document.querySelector('.row-b');
-    let rowAChildren = [...document.querySelector('.row-a').children];
-    let rowBChildren = [...document.querySelector('.row-b').children];
-
     let previousLargeContainer = document.querySelector('.large-card-container');
-    let target = event.target;
+    let dropTarget = event.target;
     let draggedItem = document.querySelector('.drag-active');
 
-      // if you're dragging a large card and staying in same row
-      if (target.parentElement === draggedItem.parentElement.parentElement &&
-      draggedItem.classList.contains('large-card')) {
+    let rowA = document.querySelector('.row-a');
+    let rowB = document.querySelector('.row-b');
+    let rows = [rowA, rowB];
 
-      draggedItem.parentNode.appendChild(target.children[0]);
-      target.appendChild(draggedItem)
-      switchLargeCardWithinRow();
-      // if you're dragging a small card and staying in same row
-    } else if (target.parentElement === draggedItem.parentElement.parentElement &&
-    !draggedItem.classList.contains('large-card')) {
-      draggedItem.parentNode.appendChild(target.children[0]);
-      target.appendChild(draggedItem)
-      // if you're dragging a large card into a different row
-    } else if (target.parentElement !== draggedItem.parentElement.parentElement
-      && draggedItem.classList.contains('large-card')) {
-        switchLargeCardToDifferentRow()
-        draggedItem.parentNode.appendChild(target.children[0]);
-        target.appendChild(draggedItem)
+    let twoRow = rows.find(row => row.children.length === 2);
+    let threeRow = rows.find(row => row.children.length === 3);
+    let twoRowChildren = [...twoRow.children];
+    let threeRowChildren = [...threeRow.children];
+
+    twoRowChildren.forEach((child, index) => child.dataset.id = index );
+    threeRowChildren.forEach((child, index) => child.dataset.id = index );
+
+
+        // small card to small card
+      if (draggedItem.classList.contains('small-card') &&
+      !dropTarget.classList.contains('large-card-container')) {
+
+        draggedItem.parentNode.appendChild(dropTarget.children[0]);
+        dropTarget.appendChild(draggedItem)
+
+        // large card to small card
+      } else if (draggedItem.classList.contains('large-card') &&
+      dropTarget.classList.contains('large-card-container')) {
+        
+      } else if (draggedItem.classList.contains('large-card')) {
+        // if in the same row
+          if (draggedItem.parentNode.parentNode === dropTarget.parentNode) {
+            draggedItem.parentNode.appendChild(dropTarget.children[0]);
+            dropTarget.appendChild(draggedItem);
+            largeToSmallWithinRow();
+        // if moving to different row
+          } else {
+            moveLargeCardToNewRow();
+          }
+
+        // small card to large card
+      } else {
+        // if in the same row
+        if (draggedItem.parentNode.parentNode === dropTarget.parentNode) {
+          draggedItem.parentNode.appendChild(dropTarget.children[0]);
+          dropTarget.appendChild(draggedItem);
+          smallToLargeWithinRow();
+        } else {
+        // if moving to different row
+          moveSmallCardToNewRow();
+        }
       }
 
-    function switchLargeCardWithinRow() {
+    function largeToSmallWithinRow() {
       if (previousLargeContainer.classList.contains('large-card-right')) {
 
         previousLargeContainer.classList.remove('large-card-right');
         previousLargeContainer.classList.remove('large-card-container');
         previousLargeContainer.classList.add('card-container');
 
-        target.classList.add('large-card-left');
-        target.classList.add('large-card-container');
-        target.classList.remove('card-container');
+        dropTarget.classList.add('large-card-left');
+        dropTarget.classList.add('large-card-container');
+        dropTarget.classList.remove('card-container');
 
       } else if (previousLargeContainer.classList.contains('large-card-left')) {
 
-        target.classList.add('large-card-right');
-        target.classList.add('large-card-container');
-        target.classList.remove('card-container');
+        dropTarget.classList.add('large-card-right');
+        dropTarget.classList.add('large-card-container');
+        dropTarget.classList.remove('card-container');
 
         previousLargeContainer.classList.remove('large-card-left');
         previousLargeContainer.classList.remove('large-card-container');
@@ -54,42 +77,83 @@ interact('.dropzone')
       }
     }
 
-    function switchLargeCardToDifferentRow() {
-      previousLargeContainer.classList.remove('large-card-left');
-      previousLargeContainer.classList.remove('large-card-right');
-      previousLargeContainer.classList.remove('large-card-container');
-      previousLargeContainer.classList.add('card-container');
-      let targetRow = target.parentNode;
+    function smallToLargeWithinRow() {
+      if (dropTarget.classList.contains('large-card-right')) {
 
-      if (targetRow === rowA) {
-        console.log('rowA');
-      } else if (targetRow === rowB) {
-        if (parseInt(target.dataset.id) === 3) {
-          target.classList.add('large-card-left');
-          rowA.appendChild(rowBChildren.slice(-1)[0])
-          console.log(rowBChildren.slice(-1)[0]);
-        } else if (parseInt(target.dataset.id) === 4) {
-          target.classList.add('large-card-left');
-          rowA.appendChild(rowBChildren.slice(0)[0])
-        } else if (parseInt(target.dataset.id) === 5) {
-          target.classList.add('large-card-right');
-          rowA.appendChild(rowBChildren.slice(0)[0])
-        }
+        twoRow.children[0].classList.add('large-card-left');
+        twoRow.children[0].classList.add('large-card-container');
+        dropTarget.classList.remove('large-card-right');
+        dropTarget.classList.remove('large-card-container');
+        dropTarget.classList.add('card-container');
+
+      } else if (dropTarget.classList.contains('large-card-left')) {
+
+        twoRow.children[1].classList.add('large-card-right');
+        twoRow.children[1].classList.add('large-card-container');
+        dropTarget.classList.remove('large-card-left');
+        dropTarget.classList.remove('large-card-container');
+        dropTarget.classList.add('card-container');
+
       }
-      
-      target.classList.add('large-card-container');
-      target.classList.remove('card-container');
     }
 
-    // if (rowAChildren.length > rowBChildren.length) {
-    //   console.log('three in row A');
-    // } else {
-    //   console.log('three in row B', rowAChildren[-1]);
-    // }
-    // console.log('target', target.children.length);
-    // console.log('item', draggedItem.parentElement.parentElement);
-    // console.log('rowA', rowA);
-    // console.log('rowB', rowB);
+    function moveLargeCardToNewRow() {
+      draggedItem.parentNode.classList.remove('large-card-container');
+      draggedItem.parentNode.classList.add('card-container');
+      draggedItem.parentNode.classList.remove('large-card-left');
+      draggedItem.parentNode.classList.remove('large-card-right');
+      dropTarget.classList.add('large-card-container');
+
+      if (dropTarget.dataset.id === "0") {
+        draggedItem.parentNode.appendChild(dropTarget.children[0]);
+        draggedItem.parentNode.parentNode.appendChild(threeRowChildren[2]);
+        dropTarget.appendChild(draggedItem);
+        dropTarget.classList.add('large-card-left')
+      } else if (dropTarget.dataset.id === "1") {
+        // console.log('second item');
+        draggedItem.parentNode.appendChild(dropTarget.children[0]);
+        draggedItem.parentNode.parentNode.appendChild(threeRowChildren[0]);
+        dropTarget.appendChild(draggedItem);
+        dropTarget.classList.add('large-card-left')
+      } else if (dropTarget.dataset.id === "2") {
+        // console.log('third item');
+        draggedItem.parentNode.appendChild(dropTarget.children[0]);
+        draggedItem.parentNode.parentNode.appendChild(threeRowChildren[0]);
+        dropTarget.appendChild(draggedItem);
+        dropTarget.classList.add('large-card-right')
+      }
+    }
+
+    function moveSmallCardToNewRow() {
+      dropTarget.classList.remove('large-card-container');
+      dropTarget.classList.remove('large-card-right');
+      dropTarget.classList.remove('large-card-left');
+      dropTarget.classList.add('card-container');
+
+      if (draggedItem.parentNode.dataset.id === "0") {
+
+        draggedItem.parentNode.appendChild(dropTarget.children[0]);
+        dropTarget.appendChild(draggedItem);
+        draggedItem.parentNode.parentNode.appendChild(threeRow.children[2]);
+        threeRow.children[0].classList.add('large-card-left');
+        threeRow.children[0].classList.add('large-card-container');
+
+      } else if (draggedItem.parentNode.dataset.id === "1") {
+
+        draggedItem.parentNode.appendChild(dropTarget.children[0]);
+        dropTarget.appendChild(draggedItem);
+        draggedItem.parentNode.parentNode.appendChild(threeRow.children[0]);
+        threeRow.children[0].classList.add('large-card-left');
+        threeRow.children[0].classList.add('large-card-container');
+
+      } else if (draggedItem.parentNode.dataset.id === "2") {
+        draggedItem.parentNode.appendChild(dropTarget.children[0]);
+        dropTarget.appendChild(draggedItem);
+        draggedItem.parentNode.parentNode.appendChild(threeRow.children[0]);
+        threeRow.children[1].classList.add('large-card-right');
+        threeRow.children[1].classList.add('large-card-container');
+      }
+    }
 
     resetItemCoordinates(draggedItem);
   })
